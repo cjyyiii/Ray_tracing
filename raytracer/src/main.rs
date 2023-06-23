78c292ca8d9de4f36d0937f8eef3afc7a944112d
cjyyiii <2269050923@qq.com>
2023-06-23 17:27:19 +0800

    colored

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -80,35 +80,26 @@ fn main() {
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
-        let ret: Color = Color::new(1.0, 0.0, 0.0);
-        return ret;
+    let t: f64 = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n: Vec3 = Vec3::unit_vector(Ray::at(&r, t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
-    //2
-    // let t: f64 = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r);
-    // if t > 0.0 {
-    //     let N: Vec3 = Vec3::unit_vector(Ray::at(&r, t) - Vec3::new(0.0, 0.0, -1.0));
-    //     return 0.5 * Color::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
-    // }
     let unit_direction: Vec3 = Vec3::unit_vector(r.dir);
-    let t: f64 = 0.5 * (unit_direction.y() + 1.0);
-    // let t = 0.5 * (unit_direction.y() + 1.0);
+    let t = 0.5 * (unit_direction.y() + 1.0);
     let ray_col: Color = Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t;
     ray_col
 }
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let oc: Vec3 = r.orig - center;
     let a: f64 = Vec3::dot(r.dir, r.dir);
     let b: f64 = 2.0 * Vec3::dot(oc, r.dir);
     let c: f64 = Vec3::dot(oc, oc) - radius * radius;
     let discriminant: f64 = b * b - 4.0 * a * c;
-    let flag: bool = discriminant > 0.0;
-    flag
-    // 2
-    // if discriminant < 0.0 {
-    //     return -1.0;
-    // } else {
-    //     return (-b - discriminant.sqrt()) / (2.0 * a);
-    // }
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
