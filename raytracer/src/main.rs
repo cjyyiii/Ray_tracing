4f9addfda1f40a2cdb3aba548a611a9d0eab3264
cjyyiii <2269050923@qq.com>
2023-06-30 02:30:10 +0800

    Standard Cornell box scene

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -208,8 +208,8 @@ fn cornell_box() -> HittableList {
     let white2 = white1.clone();
     let white3 = white2.clone();
     let white4 = white3.clone();
-    let white5 = white4.clone();
-    let white6 = white5.clone();
+    // let white5 = white4.clone();
+    // let white6 = white5.clone();
     let green = Arc::new(Lambertian::new(Color::new(0.12, 0.45, 0.15)));
     let light = Arc::new(DiffuseLight::new_col(Color::new(15.0, 15.0, 15.0)));
 
@@ -221,23 +221,23 @@ fn cornell_box() -> HittableList {
     world.add(Arc::new(XzRect::new(0.0, 555.0, 0.0, 555.0, 0.0, white)));
     world.add(Arc::new(XzRect::new(0.0, 555.0, 0.0, 555.0, 555.0, white1)));
     world.add(Arc::new(XyRect::new(0.0, 555.0, 0.0, 555.0, 555.0, white2)));
-    world.add(Arc::new(Box_::new(
-        Point3::new(130.0, 0.0, 65.0),
-        Point3::new(295.0, 165.0, 230.0),
-        white3,
-    )));
-    world.add(Arc::new(Box_::new(
-        Point3::new(265.0, 0.0, 295.0),
-        Point3::new(430.0, 330.0, 460.0),
-        white4,
-    )));
+    // world.add(Arc::new(Box_::new(
+    //     Point3::new(130.0, 0.0, 65.0),
+    //     Point3::new(295.0, 165.0, 230.0),
+    //     white3,
+    // )));
+    // world.add(Arc::new(Box_::new(
+    //     Point3::new(265.0, 0.0, 295.0),
+    //     Point3::new(430.0, 330.0, 460.0),
+    //     white4,
+    // )));
 
     world.add(Arc::new(Translate::new(
         Arc::new(RotateY::new(
             Arc::new(Box_::new(
                 Point3::new(0.0, 0.0, 0.0),
                 Point3::new(165.0, 330.0, 165.0),
-                white5,
+                white3,
             )),
             15.0,
         )),
@@ -248,7 +248,7 @@ fn cornell_box() -> HittableList {
             Arc::new(Box_::new(
                 Point3::new(0.0, 0.0, 0.0),
                 Point3::new(165.0, 165.0, 165.0),
-                white6,
+                white4,
             )),
             -18.0,
         )),
